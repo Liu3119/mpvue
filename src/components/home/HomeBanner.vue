637dbb9259b90a83d9c79d76baf9bd809a87d728
刘其亮 <13126783119@163.com>
2020-10-27 14:59:07 +0800

    上传

--- src/components/home/HomeBanner.vue
+++ src/components/home/HomeBanner.vue
@@ -3,6 +3,7 @@
     <div class="bg-img" :style="{backgroundImage: bgImg}">
       <div class="title">{{title}}</div>
       <div class="sub-title">{{subTitle}}</div>
+      <div>121212</div>
     </div>
   </div>
 </template>
